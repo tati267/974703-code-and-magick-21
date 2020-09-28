2a06aa86b80f746b4270db6d5d75e83e629e777a
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2020-09-28 21:24:57 +1000

    running test

--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,17 @@
 'use strict';
-
 const WIZARD_NAMES = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
 const WIZARD_SURENAMES = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
-const COAT_COLOR = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`,
+const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`,
   `rgb(0, 0, 0)`];
-const EYES_COLOR = [`black`, `red`, `blue`, `yellow`, `green`];
+const EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
+
+let getRandomInteger = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let getRandomArrayElement = function (arr) {
+  return arr[getRandomInteger(0, arr.length - 1)];
+};
 
 let userDialog = document.querySelector(`.setup`);
 userDialog.classList.remove(`hidden`);
@@ -17,24 +24,24 @@ let similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
 
 let wizards = [
   {
-    name: WIZARD_NAMES[0] + ` ` + WIZARD_SURENAMES[0],
-    coatColor: COAT_COLOR[0],
-    eyesColor: EYES_COLOR[0]
+    name: getRandomArrayElement(WIZARD_NAMES) + ` ` + getRandomArrayElement(WIZARD_SURENAMES),
+    coatColor: getRandomArrayElement(COAT_COLORS),
+    eyesColor: getRandomArrayElement(EYES_COLORS)
   },
   {
-    name: WIZARD_NAMES[1] + ` ` + WIZARD_SURENAMES[1],
-    coatColor: COAT_COLOR[1],
-    eyesColor: EYES_COLOR[1]
+    name: getRandomArrayElement(WIZARD_NAMES) + ` ` + getRandomArrayElement(WIZARD_SURENAMES),
+    coatColor: getRandomArrayElement(COAT_COLORS),
+    eyesColor: getRandomArrayElement(EYES_COLORS)
   },
   {
-    name: WIZARD_NAMES[2] + ` ` + WIZARD_SURENAMES[2],
-    coatColor: COAT_COLOR[2],
-    eyesColor: EYES_COLOR[2]
+    name: getRandomArrayElement(WIZARD_NAMES) + ` ` + getRandomArrayElement(WIZARD_SURENAMES),
+    coatColor: getRandomArrayElement(COAT_COLORS),
+    eyesColor: getRandomArrayElement(EYES_COLORS)
   },
   {
-    name: WIZARD_NAMES[3] + ` ` + WIZARD_SURENAMES[3],
-    coatColor: COAT_COLOR[3],
-    eyesColor: EYES_COLOR[3]
+    name: getRandomArrayElement(WIZARD_NAMES) + ` ` + getRandomArrayElement(WIZARD_SURENAMES),
+    coatColor: getRandomArrayElement(COAT_COLORS),
+    eyesColor: getRandomArrayElement(EYES_COLORS)
   }
 ];
 
