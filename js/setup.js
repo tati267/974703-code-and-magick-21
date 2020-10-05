7ad6bb0cb271c19c11b40b2024f2800fb2e37847
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2020-10-05 20:51:44 +1100

    module4-task1

--- js/setup.js
+++ js/setup.js
@@ -4,6 +4,7 @@ const WIZARD_SURENAMES = [`да Марья`, `Верон`, `Мирабелла`,
 const COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`,
   `rgb(0, 0, 0)`];
 const EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
+const FIREBALL_COLORS = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
 const NUMBER_OF_WIZARDS = 4;
 
 const getRandomInteger = (min, max) => {
@@ -45,3 +46,91 @@ wizards.forEach(function (wizard) {
 
 similarListElement.appendChild(fragment);
 userDialog.querySelector(`.setup-similar`).classList.remove(`hidden`);
+
+
+// modul4-task1
+const setup = document.querySelector(`.setup`);
+const userNameInput = setup.querySelector(`.setup-user-name`);
+const setupForm = setup.querySelector(`.setup-wizard-form`);
+const setupOpen = document.querySelector(`.setup-open`);
+const setupClose = setup.querySelector(`.setup-close`);
+const setupPlayer = setup.querySelector(`.setup-player`);
+const setupWizardCoat = setupPlayer.querySelector(`.wizard-coat`);
+const setupWizardEyes = setupPlayer.querySelector(`.wizard-eyes`);
+const setupFireball = setupPlayer.querySelector(`.setup-fireball`);
+const MIN_NAME_LENGTH = 2;
+const MAX_NAME_LENGTH = 25;
+// Открытие/закрытие окна настройки персонажа
+const onPopupEscPress = function (evt) {
+  if (evt.key === `Escape` && evt.target !== userNameInput) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+const onSetupSubmitClick = function () {
+  if (userNameInput.checkValidity()) {
+    setupForm.submit();
+  }
+};
+
+const openPopup = function () {
+  setup.classList.remove(`hidden`);
+  document.addEventListener(`keydown`, onPopupEscPress);
+  userNameInput.addEventListener(`input`, checkNameValidity);
+  userNameInput.addEventListener(`input`, onSetupSubmitClick);
+  setupPlayer.addEventListener(`click`, onSetupPlayerClick);
+};
+
+const closePopup = function () {
+  setup.classList.add(`hidden`);
+  document.removeEventListener(`keydown`, onPopupEscPress);
+  userNameInput.removeEventListener(`input`, checkNameValidity);
+  userNameInput.removeEventListener(`input`, onSetupSubmitClick);
+  setupPlayer.removeEventListener(`click`, onSetupPlayerClick);
+};
+
+setupOpen.addEventListener(`click`, function () {
+  openPopup();
+});
+
+setupOpen.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener(`click`, function () {
+  closePopup();
+});
+
+setupClose.addEventListener(`keydown`, function (evt) {
+  if (evt.key === `Enter`) {
+    closePopup();
+  }
+});
+
+// Валидация ввода имени персонажа
+const checkNameValidity = () => {
+  const valueLength = userNameInput.value.length;
+
+  if (valueLength < MIN_NAME_LENGTH) {
+    userNameInput.setCustomValidity(`'Ещё ${(MIN_NAME_LENGTH - valueLength)} симв.`);
+  } else if (valueLength > MAX_NAME_LENGTH) {
+    userNameInput.setCustomValidity(`Удалите лишние ${(valueLength - MAX_NAME_LENGTH)} симв.`);
+  } else {
+    userNameInput.setCustomValidity(``);
+  }
+  userNameInput.reportValidity();
+};
+// Изменение цвета мантии,глаз и fireball персонажа по нажатию
+const onSetupPlayerClick = function (evt) {
+  let targetElement = evt.target;
+  if (targetElement === setupWizardCoat) {
+    targetElement.style.fill = getRandomArrayElement(COAT_COLORS);
+  } else if (targetElement === setupWizardEyes) {
+    targetElement.style.fill = getRandomArrayElement(EYES_COLORS);
+  } else if (targetElement === setupFireball) {
+    targetElement.parentNode.style.background = getRandomArrayElement(FIREBALL_COLORS);
+  }
+};
